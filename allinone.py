693b35811c702fea73de755e27f585fe76bc9f67
HouseWife <chandu.thrilok@gmail.com>
2025-08-08 16:54:29 +0530

    Update allinone.py

--- allinone.py
+++ allinone.py
@@ -12,7 +12,7 @@ import io
 
 # === Streamlit Config ===
 st.set_page_config(page_title="NSE Options Analyzer", layout="wide")
-st_autorefresh(interval=1200000, key="datarefresh")  # 
+st_autorefresh(interval=120000, key="datarefresh")  # 
 
 # Define all instruments we'll analyze
 INSTRUMENTS = {
